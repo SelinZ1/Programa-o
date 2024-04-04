829e00bfbf262f9f0a935b2c02ef11ae54be886d
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-04-04 10:33:53 -0300

    #VERIFICAÇÃO DE TRIANGULO E TIPO DE TRIANGULO.py

--- #VERIFICAÇÃO DE TRIANGULO E TIPO DE TRIANGULO.py
+++ #VERIFICAÇÃO DE TRIANGULO E TIPO DE TRIANGULO.py
@@ -2,7 +2,7 @@
 lado1 = (input("Digite o tamanho do primeiro lado: "))
 lado2 = (input("Digite o tamanho do segundo lado: "))
 lado3 = (input("Digite o tamanho do terceiro lado: "))
-if (a + b < c) or (a + c < b) or (b + c < a):
+if (lado1 + lado2 < lado3) or (lado1 + lado3 < lado2) or (lado2 + lado3 < lado1):
     print ("Não é um triangulo")
 elif lado1 == lado2 and lado2 == lado3 and lado1 == lado3:
     print("É um triângulo equilátero.")
