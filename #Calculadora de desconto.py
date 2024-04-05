b2df583cac274139daa047c3dcedcf391efc02de
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-04-05 11:00:43 -0300

    #Calculadora de desconto.py

--- #Calculadora de desconto.py
+++ #Calculadora de desconto.py
@@ -1,6 +1,10 @@
-#Calculadora de desconto
-#Dou o preço e a % de desconto e o programa me retorna com o valor com desconto
-preco = float (input("Qual o preço do item ? "))
-porcentagem = float (input("Qual a % de desconto ? "))
-desconto = (preco * (porcentagem / 100 ))
-print ("O preço ficou R$" , desconto)
+# Calculadora de desconto
+# Dou o preço e a % de desconto e o programa me retorna com o valor com desconto
+
+preco = float(input("Qual o preço do item? "))
+porcentagem = float(input("Qual a % de desconto? "))
+
+desconto = preco * (porcentagem / 100)
+preco_final = preco - desconto
+
+print("O preço com desconto fica R$", preco_final)
