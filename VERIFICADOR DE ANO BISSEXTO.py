675bf116d0bebd5cf4387e36057b068ac3a82ba2
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-04-06 17:06:59 -0300

    VERIFICADOR DE ANO BISSEXTO.py

--- VERIFICADOR DE ANO BISSEXTO.py
+++ VERIFICADOR DE ANO BISSEXTO.py
@@ -1,6 +1,6 @@
 #VERIFICADOR DE ANO BISSEXTO (DIGITA UM ANO E DIZ SE É OU NAO)
 ano = int (input("Qual ano que deseja verificar : "))
-if (ano % 4 == 0 or % 100 == 0):
+if (ano%4==0 and ano%100!=0) or (ano%400==0):
     print ("Esse ano é bissexto")
 else:
     print ("Esse ano não é bissexto")
