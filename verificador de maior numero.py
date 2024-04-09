b8883d3eaaeae4abc97f5b92741cbba10bcd8ea4
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-04-09 08:20:54 -0300

    Verificador de maior numero.py

--- verificador de maior numero.py
+++ verificador de maior numero.py
@@ -4,10 +4,10 @@ num3 = float (input("Fale outro numero : "))
 if (num1 == num2 == num3):
     print ("Error 404")
 elif (num1 > num2 and num3):
-    print (num1, "É o maior")
+    print ("{num1} É o maior")
 elif (num2 > num1 and num2 > num3):
-    print (num2, "É o maior")
+    print ("{num2} É o maior")
 else :
-    print (num3, "É o maior")
+    print ("{num3} É o maior")
 
 # Verificador de maior numero :)
