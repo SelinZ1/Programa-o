8eb5a82bd2d18e793555fe75d2e47bb3aafeafc1
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-04-09 08:26:21 -0300

    #CALCULADORA DE JUROS SIMPLES.py

--- #CALCULADORA DE JUROS SIMPLES.py
+++ #CALCULADORA DE JUROS SIMPLES.py
@@ -4,3 +4,4 @@ juros = float (input ("Qual a taxa de juros ? "))
 periodo = int (input ("Qual o periodo em meses ? "))
 juroSimples = (valor * juros * periodo )
 print (f'Irá ter que pagar {juroSimples} por {periodo} meses')
+#1 - Pega o valor total 2 - Pega a taxa de juros 3 - Pega o periodo em meses 4 - Faz o calculo de juros simples 5 - Mostra na tela 
