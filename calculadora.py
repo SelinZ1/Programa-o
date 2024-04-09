5b2e4e1e8942e0f0c75c246bcde45f7c3ed2dfc1
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-04-09 08:19:18 -0300

    Calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,13 +2,13 @@ num1 = float (input("Escolha um numero : "))
 operador = str (input("Escolha um operador : "))
 num2 = float (input("Escolha outro numero : "))
 if operador == "+":
-    print ("O resultado é = ",num1 + num2)
+    print (f"O resultado é = {num1 + num2}")
 elif operador == "-":
-    print ("O resultado é = ",num1 - num2)
+    print (f"O resultado é = {num1 - num2}")
 elif operador == "*":
-    print ("O resultado é = ", num1 * num2)
+    print (f"O resultado é = {num1 * num2}")
 elif operador == "/":
-    print ("O resultado é = ", num1 / num2)
+    print (f"O resultado é =  {num1 / num2}")
 else:
     print ("Error 404")
 # Uma calculadora :)
