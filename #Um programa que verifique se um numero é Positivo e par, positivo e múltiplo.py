9682ada3a008f5a70d7ed7355f1cc4b39f3f833b
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-04-11 08:30:38 -0300

    #Um programa que verifique se um numero é Positivo e par, positivo e múltiplo.py

--- #Um programa que verifique se um numero é Positivo e par, positivo e múltiplo.py
+++ #Um programa que verifique se um numero é Positivo e par, positivo e múltiplo.py
@@ -1,17 +1,23 @@
-# Um programa que verifica se um número é Positivo e par, positivo e múltiplo de três, negativo e ímpar, Zero ou se não é nenhum desses
-numero = int(input("Fale um numero : "))
+print('''Bem-Vindo ao Verificador de números:
+1 - Número Par 
+2 - Número Ímpar
+3 - Número Múltiplo de 3
+4 - Positivo
+5 - Negativo''')
+
+numero = float(input("Qual número deseja verificar? "))
 
 if numero == 0:
     print("O número é zero.")
-
 elif numero > 0:
     if numero % 2 == 0 and numero % 3 == 0:
         print(f"O número {numero} é positivo, par e múltiplo de 3.")
     elif numero % 2 == 0:
         print(f"O número {numero} é positivo e par, mas não é múltiplo de 3.")
+    elif numero % 3 == 0:
+         print(f"O número {numero} é positivo e múltiplo de 3.")
     else:
         print(f"O número {numero} é positivo e ímpar.")
-
 else:  # Números negativos
     if numero % 2 == 0:
         print(f"O número {numero} é negativo e par.")
