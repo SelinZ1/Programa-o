f6eff07839ce45716f47ec2b51c894dc3b63a8e2
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-05-02 08:36:17 -0300

    #CALCULO DO PERIMETRO E AREA DE UM RETANGULO.py

--- #CALCULO DO PERIMETRO E AREA DE UM RETANGULO.py
+++ #CALCULO DO PERIMETRO E AREA DE UM RETANGULO.py
@@ -1,6 +1,8 @@
 #CALCULO DO PERIMETRO E AREA DE UM RETANGULO (PERIMETRO 2 x Largura x altura)
 altura = float (input("Qual a altura do retangulo ? "))
 largura = float (input("Qual a largura do retangulo ? "))
+area = (largura * altura)
 perimetro = (2 * (largura + altura))
+print (f"A area é = {area}")
 print (f"O perimetro é =  {perimetro}")
 # 1 - Pega a altura do retangulo 2 - Pega a largura do retangulo 3 - Faz a conta 4 - Mostra o resultado na tela
