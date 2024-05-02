b055f6d45508038c8888a6c2de77dd012b510c5d
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-05-02 08:37:44 -0300

    #VERIFICAÇÃO DE TRIANGULO E TIPO DE TRIANGULO.py

--- #VERIFICAÇÃO DE TRIANGULO E TIPO DE TRIANGULO.py
+++ #VERIFICAÇÃO DE TRIANGULO E TIPO DE TRIANGULO.py
@@ -1,7 +1,7 @@
 #VERIFICAÇÃO DE TRIANGULO E TIPO DE TRIANGULO ( DIZER SE COM OS 3 COMPRIMENTOS QUE VOCE FORNECEU É POSSIVEL FORMAR UM TRIANGULO) E DIZER DEPOIS QUAL E O TIPO DO TRIANGULO ( EQUILATERO, ISOCELES E ESCALENO)
-lado1 = (input("Digite o tamanho do primeiro lado: "))
-lado2 = (input("Digite o tamanho do segundo lado: "))
-lado3 = (input("Digite o tamanho do terceiro lado: "))
+lado1 = float (input("Digite o tamanho do primeiro lado: "))
+lado2 = float (input("Digite o tamanho do segundo lado: "))
+lado3 = float (input("Digite o tamanho do terceiro lado: "))
 if (lado1 + lado2 < lado3) or (lado1 + lado3 < lado2) or (lado2 + lado3 < lado1):
     print ("Não é um triangulo")
 elif lado1 == lado2 and lado2 == lado3 and lado1 == lado3:
